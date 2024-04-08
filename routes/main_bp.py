d1397c4ac9740c18c932d5e84db023529489a677
Rashay01 <101921464+Rashay01@users.noreply.github.com>
2024-04-08 23:30:11 +0200

    adding the passing of user

--- routes/main_bp.py
+++ routes/main_bp.py
@@ -1,18 +1,19 @@
 from flask import Blueprint, render_template
+from app import lg_user
 
 main_bp = Blueprint("main", __name__)
 
 @main_bp.route("/")
 def home():
-    return render_template("landing.html", curr_page="home")
+    return render_template("landing.html", curr_page="home", user=lg_user)
 
 
 @main_bp.route("/about/")
 def about():
-    return render_template("about.html", curr_page="about")
+    return render_template("about.html", curr_page="about", user=lg_user)
 
 
 @main_bp.route("/contact")
 def contact():
-    return render_template("contact.html", curr_page="contact")
+    return render_template("contact.html", curr_page="contact", user=lg_user)
 

165f758ea5ea151986d8e193ccf97cf5129fd26a
Rashay01 <101921464+Rashay01@users.noreply.github.com>
2024-04-21 21:22:49 +0200

    Removed unused imports

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, url_for
+from flask import Flask
 from sqlalchemy.sql import text
 from dotenv import load_dotenv
 from flask_login import LoginManager
